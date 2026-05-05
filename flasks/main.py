51af251a07f806adbda4044e8466051277bb258d
djibril99 <djibyba05@live.fr>
2026-05-05 10:06:00 +0200

    update

--- flasks/main.py
+++ flasks/main.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify , Response , render_template
 import psycopg2
-import os
-import json
 from datetime import datetime
-import matplotlib.pyplot as plt
-import io, base64
+import io
 import csv
+from flask import redirect, url_for
 
 
 app = Flask(__name__)
@@ -66,7 +64,7 @@ def insert_capteur(capteur_id, raw_val, filt_val):
 # =========================
 # RECEIVE DATA ESP32
 # =========================
-@app.route("/api/data", methods=["POST"])
+@app.route("/api/data", methods=["POST"]) #attention utisliser par l'esp32 pour envoyer les données
 def recevoir():
     data = request.get_json()
 
@@ -81,34 +79,7 @@ def recevoir():
 
     return jsonify({"status": "ok", "saved": list(data.keys())})
 
-# =========================
-# ALL DATA API
-# =========================
-@app.route("/api/all")
-def all_data():
 
-    conn = get_conn()
-    cur = conn.cursor()
-
-    cur.execute("""
-    SELECT id, capteur_id, raw_value, filtered_value, created_at
-    FROM mesures
-    ORDER BY id DESC
-    """)
-
-    rows = cur.fetchall()
-    conn.close()
-
-    return jsonify([
-        {
-            "id": r[0],
-            "capteur": r[1],
-            "raw": r[2],
-            "filtered": r[3],
-            "date": r[4]
-        }
-        for r in rows
-    ])
 @app.route("/api/data2" , methods=["GET"])
 def api_data():
     capteur = request.args.get("capteur", "ALL")
@@ -177,101 +148,6 @@ def one_capteur(capteur_id):
 # =========================
 # DASHBOARD
 # =========================
-import matplotlib.dates as mdates
-
-@app.route("/dashboard")
-def dashboard():
-
-    capteur = request.args.get("capteur", "ALL")
-    type_data = request.args.get("type", "raw")
-
-    conn = get_conn()
-    cur = conn.cursor()
-
-    cur.execute("SELECT DISTINCT capteur_id FROM mesures ORDER BY capteur_id")
-    capteurs_db = [c[0] for c in cur.fetchall()]
-
-    plt.figure()
-
-    if capteur == "ALL":
-
-        for c in capteurs_db:
-
-            cur.execute("""
-                SELECT raw_value, filtered_value, created_at
-                FROM mesures
-                WHERE capteur_id=%s
-                ORDER BY created_at ASC
-            """, (c,))
-
-            rows = cur.fetchall()
-
-            if not rows:
-                continue
-
-            raw = [r[0] for r in rows]
-            filt = [r[1] for r in rows]
-            dates = [r[2] for r in rows]
-
-            if type_data == "ALL":
-                plt.plot(dates, raw, label=f"{c} raw")
-                plt.plot(dates, filt, label=f"{c} filtered")
-            elif type_data == "filtered":
-                plt.plot(dates, filt, label=f"{c} filtered")
-            else:
-                plt.plot(dates, raw, label=f"{c} raw")
-
-        plt.legend()
-
-    else:
-
-        cur.execute("""
-            SELECT raw_value, filtered_value, created_at
-            FROM mesures
-            WHERE capteur_id=%s
-            ORDER BY created_at ASC
-        """, (capteur,))
-
-        rows = cur.fetchall()
-
-        if rows:
-            raw = [r[0] for r in rows]
-            filt = [r[1] for r in rows]
-            dates = [r[2] for r in rows]
-
-            if type_data == "ALL":
-                plt.plot(dates, raw, label="raw")
-                plt.plot(dates, filt, label="filtered")
-                plt.legend()
-            elif type_data == "filtered":
-                plt.plot(dates, filt)
-            else:
-                plt.plot(dates, raw)
-
-    conn.close()
-
-    # Format heure
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-    plt.gcf().autofmt_xdate()
-
-    plt.title(f"Dashboard {capteur}")
-    plt.xlabel("time")
-    plt.ylabel("value")
-
-    img = io.BytesIO()
-    plt.savefig(img, format="png")
-    plt.close()
-    img.seek(0)
-
-    graph = base64.b64encode(img.getvalue()).decode()
-
-    return render_template(
-        "dashboard.html",
-        graph=graph,
-        capteur=capteur,
-        type_data=type_data,
-        capteurs_db=capteurs_db
-    )
 @app.route("/dashboard2")
 def dashboard2():
     return render_template("dashboard2.html")
@@ -324,7 +200,7 @@ def export_csv():
             "Content-Disposition": "attachment; filename=mesures.csv"
         }
     )
-from flask import redirect, url_for
+
 
 @app.route("/clear", methods=["POST"])
 def clear():
@@ -349,7 +225,8 @@ def clear():
 # =========================
 @app.route("/")
 def home():
-    return "Flask + PostgreSQL Render OK"
+    #url de dashboard
+    return redirect(url_for("dashboard2"))
 # =========================
 # START
 # =========================
